e3a96f35accd5c93a4708230591d54b68a975b52
Alen Hujdur <hujduralen@gmail.com>
2017-03-03 20:30:48 +0100

    ok

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,18 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.all
   end
+
+  def new
+    @location = Location.new(params[:location])
+  end
+
+  def create
+    if @location.save
+      flash[:notice] = "Location saved!"
+    end
+  end
+
+  private
+
+  def 
 end
