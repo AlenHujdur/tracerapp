ffea3f8cd47692ad5270422177bbda367f72d7ad
AlenHujdur <hujduralen@gmail.com>
2018-03-20 22:39:20 +0100

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,36 @@ class UsersController < ApplicationController
       @users = User.all
     end
 
-    def show
-    end
-
     def new
       @user = User.new
     end
 
     def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Your account has been created successfully"
+        session[:user_id] = @user.id
+        redirect_to locations_path
+      else
+        render 'new'
+      end
+    end
+
+    def update
+      if @user.update(user_params)
+        flash[:success] = "Your profile has been updated succesfully"
+        redirect_to user_path(@user)
+      else
+        render 'edit'
+      end
+    end
     end
 
     def destroy
+    end
+
+    def show
+      @user = User.find(params[:id])
     end
 
     private
