e5bc79df54868032d77e90816f52fa21caf64ce2
AlenHujdur <hujduralen@gmail.com>
2018-03-26 22:07:57 +0200

    form partial fixed

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+#devise
+gem 'devise'
 # ruby v 2.5
 ruby "~> 2.5.0"
 # db for testing sqlite3
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  #devise_for :users
   root 'locations#index'
-  resources :locations
-  resources :users
-
   get '/login', to: "logins#new"
   post '/login', to: "logins#create"
   get '/logout', to: "logins#destroy"
+  resources :locations
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
