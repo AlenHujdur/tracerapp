fec7d77b7da4692db1c590a01b0d867c5d814d63
AlenHujdur <hujduralen@gmail.com>
2018-03-26 22:38:06 +0200

    locations controller is edited

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,8 @@
 class LocationsController < ApplicationController
   #skip_before_action :verify_authenticity_token
+
+  before_action :require_same_user, only: [:edit, :update]
+
   def index
     @locations = Location.all
     respond_to do |format|
@@ -22,19 +25,31 @@ class LocationsController < ApplicationController
 
   def create
     @location = Location.new(location_params)
-    @location.save
-    flash[:notice] = "Location saved!"
+    @location.chef = current_user
+    if @location.save
+      flash[:notice] = "Location saved!"
       respond_to do |format|    
       format.html { redirect_to location_path(@location), notice: 'Location was successfully created.' }
       #format.js
       end
+    else
+       render 'new'
+    end
     #   format.xml {render xml: @locations}
     #   format.json {render json: @locations}
     #   format.csv {render csv: @locations}
-    end
   end
 
   private
   def location_params
-    params.require(:location).permit(:latitude, :longitude, :info)
+    params.require(:location).permit(:latitude, :longitude, :info, :user_id)
   end
+
+  def require_same_user
+    if current_user != @location.user
+      flash[:danger] = "You can only edit your own recipes"
+      redirect_to locations_path
+    end
+  end
+
+end
