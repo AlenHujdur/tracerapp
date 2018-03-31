e6cea9cef88403324fb080f6737d7421b64fe275
AlenHujdur <hujduralen@gmail.com>
2018-03-31 23:57:05 +0200

    new location form*

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,16 +72,3 @@ function GOOGLEAPI(){
 
 }
 //});
-
-var camera = new JpegCamera("#camera");
-
-var snapshot = camera.capture();
-
-snapshot.show(); // Display the snapshot
-
-snapshot.upload({api_url: "/upload_image"}).done(function(response) {
-  response_container.innerHTML = response;
-  this.discard(); // discard snapshot and show video stream again
-}).fail(function(status_code, error_message, response) {
-  alert("Upload failed with status " + status_code);
-});
