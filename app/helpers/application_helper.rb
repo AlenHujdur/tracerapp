9332e26d09f4cffdd1f18f1602b6dcb41513ef40
AlenHujdur <hujduralen@gmail.com>
2018-04-12 16:21:41 +0200

    helper method created for images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def picture_for(location, options = { size: 80 })
+      size = options[:size]
+      if location.image.url == nil
+        return "No image!"
+      else
+        image_tag(location.image.url, alt: location.info)
+      end
+  end
+
 end
