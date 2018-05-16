df2e566883ca9b88f2db79983ed989056a61d20b
AlenHujdur <hujduralen@gmail.com>
2018-05-16 16:02:19 +0200

    country json return is not working for all cities --need to check json structure

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -23,8 +23,6 @@ class LocationsController < ApplicationController
     @location = Location.find(location_id)
       lat = @location.latitude
       long = @location.longitude
-    # @locat = Location.loc(lat,long)
-    # @get_adress = Location.get_address
     @map = @location.newmap(lat,long)
   end
 
--- lib/get_location.rb
+++ lib/get_location.rb
@@ -10,28 +10,58 @@ module GetLocation
     end 
     def location
       @m = Map.new(@lat,@long)
-      self.class.get("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyApzU3b0GAQzSzAUT9wtDNZtTZXi9WXi9Q&latlng=#{@lat},#{@long}&sensor=false")
+      
+      if @response = self.class.get("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyApzU3b0GAQzSzAUT9wtDNZtTZXi9WXi9Q&latlng=#{@lat},#{@long}&sensor=false")
+        @response
+      else
+        case response.code
+          when 404
+            p "Not found!"
+          when 500...600
+            p "Error: #{response.code}" 
+        end
+      end
+    end    
+    
+    def get_country
+      c = location
+      if c.nil?
+        p c.response.code
+      elsif c['results'][0]['address_components'][6]['long_name'].blank? 
+        return "No country name"  
+      else
+        country = "#{c['results'][0]['address_components'][6]['long_name']}"
+        return country
+      end
     end
-        
-    # @city = @m.location['results'][2]['address_components'][1]['long_name']
+    def get_region
+      r = location
+      region = "#{r['results'][2]['address_components'][2]['long_name']}"
+    end
+    def get_city
+      c = location
+      city = "#{c['results'][2]['address_components'][1]['long_name']}"
+    end
     def get_address
-      @m = location #Map.new(@lat,@long)  
-      #location = self.class.get("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyApzU3b0GAQzSzAUT9wtDNZtTZXi9WXi9Q&latlng=#{@lat},#{@long}&sensor=false")
-      self.class.get("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyApzU3b0GAQzSzAUT9wtDNZtTZXi9WXi9Q&latlng=#{@lat},#{@long}&sensor=false")      
-      @address = "#{@m['results'][0]['address_components'][1]['long_name']}, #{@m['results'][0]['address_components'][0]['long_name']}, #{@m['results'][0]['address_components'][2]['long_name']}"
+      m = location    
+      address = "#{m['results'][0]['address_components'][1]['long_name']}, #{m['results'][0]['address_components'][0]['long_name']}, #{m['results'][0]['address_components'][2]['long_name']}"
+    end
+
+    def get_details
+      return "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyApzU3b0GAQzSzAUT9wtDNZtTZXi9WXi9Q&latlng=#{@lat},#{@long}&sensor=false"
+    end
+
+    def get_map_type
+      return "http://maps.googleapis.com/maps/api/staticmap?&size=600x600&sensor=false&zoom=16&markers=#{@lat}%2C#{@long}"
+    end
+
+    def get_satellite_type
+      return "http://maps.googleapis.com/maps/api/staticmap?maptype=satellite&size=600x600&sensor=false&zoom=16&markers=#{@lat}%2C#{@long}"
+    end
+
+    def get_image_location
+      return "https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyApzU3b0GAQzSzAUT9wtDNZtTZXi9WXi9Q&maxwidth=400&latlng=#{@lat},#{@long}"
     end
   end
 
-    
-    # p "Adress: #{m.location['results'][0]['address_components'][1]['long_name']}, #{m.location['results'][0]['address_components'][0]['long_name']}, #{m.location['results'][0]['address_components'][2]['long_name']}"
-    # p "City: #{m.location['results'][2]['address_components'][1]['long_name']}"
-    # p "Region: #{m.location['results'][2]['address_components'][2]['long_name']}"
-    # p "Country: #{m.location['results'][0]['address_components'][6]['long_name']}"
-
-
-    # p "City: #{m.location['results'][2]['address_components'][1]['long_name']}"
-    # region = m.location['results'][2]['address_components'][2]['long_name']
-    # country = m.location['results'][0]['address_components'][6]['long_name']
-
-    # p city.to_s
 end
