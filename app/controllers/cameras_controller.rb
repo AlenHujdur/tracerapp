0140d4131844818eebcbc9ccd769b2c9ee259e20
Alen Hujdur <alen@sigura.eu>
2018-08-01 00:06:28 +0200

    location added

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -6,11 +6,26 @@ class CamerasController < ApplicationController
 
   def create
     @camera = Camera.new(camera_params)
-    @camera.save!
+    @location = Location.new(location_params)
+    @camera.location.save!
+    @location.user = current_user
+    if @location.save
+      flash[:notice] = "Location saved!"
+      respond_to do |format|    
+      format.html { redirect_to location_path(@location), notice: 'Location was successfully created.' }
+      end
+    else
+       render 'new'
+    end
+    
   end
   private
   def camera_params
     params.require(:camera).permit(:image, :map_image)
+  end
+
+  def location_params
+    params.require(:location).permit(:latitude, :longitude, :info, :image, :map_image, :user_id)
   end
 
   def require_same_user
