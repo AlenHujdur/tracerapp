6caee903bcd56f274856ce2b3621754e703fe99b
Alen Hujdur <alen@sigura.eu>
2018-07-31 23:58:00 +0200

    create method on camera controller

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -4,9 +4,20 @@ class CamerasController < ApplicationController
     @camera = Camera.new
   end
 
+  def create
+    @camera = Camera.new(camera_params)
+    @camera.save
+  end
   private
   def camera_params
     params.require(:camera).permit(:image, :map_image)
+  end
+
+  def require_same_user
+    if current_user != @location.user
+      flash[:danger] = "You can only edit your own recipes"
+      redirect_to locations_path
+    end
   end
 
 end
